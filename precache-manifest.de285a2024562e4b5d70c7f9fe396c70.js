64471704501380b1bc01163173468a22bac89f3f
mohammad nour itani <mohammad.nour.itani@gmail.com>
2020-03-22 17:47:56 +0200

    Updates

---
+++ precache-manifest.de285a2024562e4b5d70c7f9fe396c70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d3a775cfc1024784e1bf8f3e416dc03",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e806fdf9a8ec00a7b58c",
+    "url": "/monsters-rolodex/static/css/main.1f98d119.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e806fdf9a8ec00a7b58c",
+    "url": "/monsters-rolodex/static/js/main.7e75d354.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
